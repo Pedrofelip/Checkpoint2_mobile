0aafaaf875fbf2dced60f70352a96d2935b70e9a
pedrofelip <pedrofelipbarros@gmail.com>
2024-04-10 18:51:02 -0300

    projeto de mobile quase finalizado 2

---
+++ app/src/main/java/br/com/pedrofelip/checkpoint2mad/MainActivity.kt
@@ -0,0 +1,54 @@
+package br.com.pedrofelip.checkpoint2mad
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnAluno = findViewById<Button>(R.id.btn_aluno)
+        val btnCalculadora = findViewById<Button>(R.id.btn_calculadora)
+        val btnTelefonia = findViewById<Button>(R.id.btn_contaTelefonica)
+
+        btnAluno.setOnClickListener{
+            val artDialogBuilder = AlertDialog.Builder(this@MainActivity)
+
+            artDialogBuilder.setTitle("Desenvolvido por:")
+            artDialogBuilder.setMessage(
+                    "Pedro Felipe\nRM: 98093")
+            artDialogBuilder.setCancelable(false)
+            artDialogBuilder.setPositiveButton("OK"){_,_ ->
+                Toast.makeText(this@MainActivity, "você viu o nome do desenvolvedor", Toast.LENGTH_LONG).show()
+            }
+            val alertDialogBox = artDialogBuilder.create()
+            alertDialogBox.show()
+        }
+
+        btnCalculadora.setOnClickListener{
+            IrParaCalculadora()
+        }
+        btnTelefonia.setOnClickListener{
+            IrParaTelefonia()
+        }
+    }
+
+    private fun IrParaCalculadora(){
+        val calculadora = Intent(this, Calculadora::class.java)
+        startActivity(calculadora)
+    }
+
+    private fun IrParaTelefonia(){
+        val telefonia = Intent(this, Telefonia::class.java)
+        startActivity(telefonia)
+    }
+}
